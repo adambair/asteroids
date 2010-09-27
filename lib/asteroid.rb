2e49b462c826dd6ccc7894417f098c759fa5dc11
Ariel H. Pillet <apillet@gmail.com>
2010-09-27 02:00:54 -0300

    Removes unnecessary condition

--- lib/asteroid.rb
+++ lib/asteroid.rb
@@ -36,8 +36,7 @@ class Asteroid
   def fragment
     return [] unless next_size
     asteroids = Array.new(2) { Asteroid.new(@window, next_size) }
-    speed = next_size == 'large' ? 2 : 2.5
-    asteroids.collect {|asteroid| asteroid.setup(@x, @y, rand(0)*speed+0.5) }
+    asteroids.collect {|asteroid| asteroid.setup(@x, @y, rand(0)*2.5+0.5) }
   end
 
   def next_size

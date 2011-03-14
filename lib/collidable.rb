f7f861a4ee4a1eee1e86cfee2d1f3a4163823526
Ariel H. Pillet <apillet@gmail.com>
2011-03-14 18:03:19 -0200

    Small refactoring on Collidable hitbox

--- lib/collidable.rb
+++ lib/collidable.rb
@@ -1,8 +1,10 @@
 module Collidable
   def hitbox
-    hitbox_x = ((@x - @image.width/2).to_i..(@x + @image.width/2.to_i)).to_a
-    hitbox_y = ((@y - @image.width/2).to_i..(@y + @image.width/2).to_i).to_a
-    {:x => hitbox_x, :y => hitbox_y}
+    { :x => ((@x - half_size).to_i..(@x + half_size).to_i).to_a,
+      :y => ((@y - half_size).to_i..(@y + half_size).to_i).to_a }
+  end
+  
+  def half_size
+    @image.width / 2
   end
 end
-

aefd081bfe1fd6b1e9269e337841e18727e27660
Ariel H. Pillet <apillet@gmail.com>
2011-03-17 09:57:21 -0200

    Added missing require

--- asteroids.rb
+++ asteroids.rb
@@ -1,12 +1,13 @@
 require 'rubygems'
 require 'gosu'
 
-require 'lib/collidable'
-require 'lib/player'
-require 'lib/asteroid'
-require 'lib/projectile'
-require 'lib/particle'
-require 'lib/game'
+require './lib/collidable'
+require './lib/game_object'
+require './lib/player'
+require './lib/asteroid'
+require './lib/projectile'
+require './lib/particle'
+require './lib/game'
 
 window = GameWindow.new
 window.show

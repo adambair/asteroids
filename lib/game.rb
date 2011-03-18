077bde1ce8d382c3e8f0e128023544d41610c024
Ariel H. Pillet <apillet@gmail.com>
2011-03-18 00:37:08 -0200

    Refactored Game class.

--- lib/game.rb
+++ lib/game.rb
@@ -2,15 +2,39 @@ class GameWindow < Gosu::Window
   POINTS = { 'large'  => 20,
              'medium' => 50,
              'small'  => 100 }
-  
+
   def initialize
     super(800, 600, false)
     @life_image = Gosu::Image.new(self, "assets/player.png", false)
     @font = Gosu::Font.new(self, 'Inconsolata-dz', 24)
-    @game_in_progress = false
+    @state = :title
     title_screen
   end
 
+  # Game States
+  #
+  def title?
+    @state == :title
+  end
+  
+  def playing?
+    @state == :playing
+  end
+
+  def play!
+    @state = :playing
+  end
+
+  def game_over?
+    @state == :game_over
+  end
+
+  def game_over!
+    @state = :game_over
+  end
+
+  # Prepare Title Screen
+  #
   def title_screen
     @asteroids = spawn_asteroids(4)
     @asteroids += @asteroids[0].kill
@@ -18,45 +42,60 @@ class GameWindow < Gosu::Window
     @asteroids += @asteroids.last.kill
   end
 
+  # Prepare Game Screen
+  #
   def setup_game
     @player = Player.new(self)
     @level = 1
     @score = 0
     @asteroid_count = 3
     @asteroids = spawn_asteroids(@asteroid_count)
-    @projectiles, @particles = [], []
-    @game_in_progress = true
+    @particles = []
+    @projectiles = []
+    play!
   end
 
-  # 60 times per second
+  # Input
+  #
+  def button_down(id)
+    close if id == Gosu::KbQ || id == Gosu::KbEscape
+    shoot if id == Gosu::KbSpace
+    
+    if title? and id == Gosu::KbS
+      setup_game
+    end
+
+    if game_over? and id == Gosu::KbR
+      title_screen
+      setup_game
+    end
+  end
+
+  # Game Logic
+  #
   def update
-    if button_down? Gosu::KbS
-      setup_game unless @game_in_progress
+    handle(@asteroids)
+
+    if playing?
+      control_player
+      handle(@projectiles)
+      handle(@particles)
+      detect_collisions
+
+      next_level unless @asteroids.any?
     end
+  end
 
-    if button_down? Gosu::KbR
-      title_screen unless @game_in_progress == false
-      @game_in_progress = false
-    end
-
-    @asteroids.each{|asteroid| asteroid.move}
-    @asteroids.reject!{|asteroid| asteroid.dead?}
-
-    return unless @game_in_progress
-    return if @player.dead?
-
-    control_player
-
+  def control_player
+    @player.turn_left  if button_down?(Gosu::KbLeft)
+    @player.turn_right if button_down?(Gosu::KbRight)
+    @player.accelerate if button_down?(Gosu::KbUp)
     @player.move
+  end
 
-    @projectiles.each{|projectile| projectile.move}
-    @projectiles.reject!{|projectile| projectile.dead?}
-
-    @particles.each{|particle| particle.move}
-    @particles.reject!{|particle| particle.dead?}
-
-    detect_collisions
-    next_level if @asteroids.size == 0 
+  def handle things
+    things.each    {|thing| thing.move  }
+    things.reject! {|thing| thing.dead? }
   end
 
   def next_level
@@ -65,21 +104,61 @@ class GameWindow < Gosu::Window
     @asteroids = spawn_asteroids(@asteroid_count)
   end
 
-  # happens immediately after each iteration of the update method
+  def shoot
+    if can_shoot?
+      @projectiles << @player.shoot
+    end
+  end
+
+  def can_shoot?
+     playing? && @projectiles.size < 5
+  end
+
+  def detect_collisions
+    @asteroids.each do |asteroid|
+      @player.kill if asteroid.collides_with?(@player)
+
+      @projectiles.each do |projectile|
+        if asteroid.collides_with?(projectile)
+          projectile.kill
+          30.times {@particles << Particle.new(self, asteroid)}
+          @score += POINTS[asteroid.size]
+          @asteroids += asteroid.kill
+        end
+      end
+    end
+  end
+
+  def spawn_asteroids(count=3, size='large')
+    Array.new(count) { Asteroid.new(self, :size => size) }
+  end
+
+  # Render
+  #
   def draw
-    # @background_image.draw(0, 0, 0)
     @asteroids.each{|asteroid| asteroid.draw}
-    title_text unless @game_in_progress
-    return unless @game_in_progress
-    game_over_text if @player.lives <= 0
+
+    case @state
+    when :title then
+      title_text
+    when :playing then
+      @player.draw
+      @projectiles.each{|projectile| projectile.draw}
+      @particles.each{|particle| particle.draw}
+      show_gui
+      game_over! if @player.dead?
+    when :game_over then
+      game_over_text
+    end
+  end
+
+  # GUI
+  #
+  def show_gui
     score_text
     level_text
-    return if @player.dead?
-    @player.draw
-    @projectiles.each{|projectile| projectile.draw}
-    @particles.each{|particle| particle.draw}
     draw_lives
-  end 
+  end
 
   def score_text
     write @score, 10, 10
@@ -89,6 +168,17 @@ class GameWindow < Gosu::Window
     write @level, 610, 10
   end
 
+  def draw_lives
+    return unless @player.lives > 0
+    x = 10
+    @player.lives.times do 
+      @life_image.draw(x, 40, 0)
+      x += 20
+    end
+  end
+
+  # Texts
+  #
   def title_text
     center "ASTEROIDS", 120, 2.8
     center "press 's' to start", 320
@@ -108,56 +198,5 @@ class GameWindow < Gosu::Window
   def center text, y, factor_x = 1 
     x = (width - @font.text_width(text, factor_x)) / 2
     write(text, x, y, factor_x, factor_x)
-  end
-
-  def draw_lives
-    return unless @player.lives > 0
-    x = 10
-    @player.lives.times do 
-      @life_image.draw(x, 40, 0)
-      x += 20
-    end
-  end
-
-  def button_down(id)
-    close if id == Gosu::KbQ || id == Gosu::KbEscape
-    shoot if id == Gosu::KbSpace
-  end
-
-  def control_player
-    @player.turn_left  if button_down?(Gosu::KbLeft)
-    @player.turn_right if button_down?(Gosu::KbRight)
-    @player.accelerate if button_down?(Gosu::KbUp)
-  end
-
-  def shoot
-    if can_shoot?
-      @projectiles << @player.shoot
-    end
-  end
-
-  def can_shoot?
-     @game_in_progress && @projectiles.size < 5
-  end
-
-  def detect_collisions
-    @asteroids.each do |asteroid|      
-      if asteroid.collides_with?(@player)
-        @player.kill
-      end
-
-      @projectiles.each do |projectile|
-        if asteroid.collides_with?(projectile)
-          projectile.kill
-          30.times {@particles << Particle.new(self, asteroid)}
-          @score += POINTS[asteroid.size]
-          @asteroids += asteroid.kill
-        end
-      end
-    end
-  end
-
-  def spawn_asteroids(count=3)
-    Array.new(count) { Asteroid.new(self) }
   end
 end
